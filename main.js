442ac0ea46b4220d4c10987791908ded3b89cc90
atillaahun <szikora.atilla@gmail.com>
2016-05-31 17:00:36 +0200

    JSON sample code added.

--- main.js
+++ main.js
@@ -21,7 +21,22 @@ $(document).ready(function() {
             $('body').append(contact.sum()); } }
 
     function fillContactData() {
-
+        var url = "http://www.filltext.com/?callback=?";
+        $.getJSON( url, {
+            'rows': 5,
+            'fname': '{firstName}',
+            'lname': '{lastName}',
+            'tel': '{phone|format}'
+        })
+            .done(function( data ) {
+                $.each( data, function( i, item ) {
+                    var html =
+                        "<td>" + item.fname + "</td>" +
+                        "<td>" + item.lname + "</td>" +
+                        "<td>" + item.tel + "</td>";
+                    $("<tr/>").html(html).appendTo("#records");
+                });
+            });
     }
 
     fillContactData();
