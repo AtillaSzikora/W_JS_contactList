2ee546836e0b9eb506c991fb39ec7890230489e0
atillaahun <szikora.atilla@gmail.com>
2016-06-01 10:36:12 +0200

    $.when.then fucks up my life.

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 $(document).ready(function() {
     var json;
+    var response;
+    var xmlHttp = new XMLHttpRequest();
+    var url = "http://www.filltext.com/?rows=1&" +
+        "firstName={firstName}&lastName={lastName}&" +
+        "city={city}&address={streetAddress}&" +
+        "email={email}&phone={phone|format}";
 
-    function getJSONFromUrl() {
-        var xmlHttp = new XMLHttpRequest();
-        var url = "http://www.filltext.com/?rows=1&" +
-            "firstName={firstName}&lastName={lastName}&" +
-            "city={city}&address={streetAddress}&" +
-            "email={email}&phone={phone|format}";
+    function stateChange() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            response = xmlHttp.responseText;
+            json = JSON.parse(response);
+            console.log("Elso"); } }
 
-        var stateChange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                var response = xmlHttp.responseText;
-                json = JSON.parse(response); } };
-        xmlHttp.addEventListener("readystatechange", stateChange);
-        xmlHttp.open("GET", url, true);
-        xmlHttp.send(); }
+    xmlHttp.addEventListener("readystatechange", stateChange);
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
 
     function createCards() {
         for (var i = 0; i < 5; i++) {
             $('body').append(contact.sum()); } }
 
-    getJSONFromUrl();
-
     var contact = {
-        firstName: json[0],
+        firstName: "Atilla",
         lastName: "Szikora",
         city: "Miskolc",
         address: "Katalin u. 7",
         email: "szikora.atilla@gmail.com",
         phone: "+36309728668",
-
         sum: function () {
             return "<div>" +
                 "<p>" + this.firstName + " " + this.lastName + "</p>" +
@@ -38,5 +36,6 @@ $(document).ready(function() {
                 "<p>" + this.phone + "</p>" +
                 "</div>" } };
 
+    $.when(stateChange()).then(console.log("Masodik"));
     createCards();
 });
