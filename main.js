56d7d98af9f0acf480f6262ea43b95dbee7796c4
atillaahun <szikora.atilla@gmail.com>
2016-06-01 16:36:10 +0200

    Still can't access the JSON object.

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
 $(document).ready(function() {
-    var json;
-    var response;
     var xmlHttp = new XMLHttpRequest();
     var url = "http://www.filltext.com/?rows=1&" +
         "firstName={firstName}&lastName={lastName}&" +
         "city={city}&address={streetAddress}&" +
         "email={email}&phone={phone|format}";
 
-    function stateChange() {
+    function httpStateChange() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            response = xmlHttp.responseText;
-            json = JSON.parse(response);
-            console.log("Elso"); } }
+            var response = xmlHttp.responseText;
+            var json = JSON.parse(response);
+            createContact(json); } }
 
-    xmlHttp.addEventListener("readystatechange", stateChange);
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send();
+    function createContact(json) {
+        var contact = {
+            firstName: json.firstName,
+            lastName: "Szikora",
+            city: "Miskolc",
+            address: "Katalin u. 7",
+            email: "szikora.atilla@gmail.com",
+            phone: "+36309728668",
+            sum: function () {
+                return "<div>" +
+                    "<p>" + this.firstName + " " + this.lastName + "</p>" +
+                    "<p>" + this.city + ", " + this.address + "</p>" +
+                    "<p>" + this.email + "</p>" +
+                    "<p>" + this.phone + "</p>" +
+                    "</div>" } };
+        return contact.sum(); }
 
     function createCards() {
-        for (var i = 0; i < 5; i++) {
-            $('body').append(contact.sum()); } }
+        for (var i = 0; i < 6; i++) {
+            $('body').append(createContact()); } }
 
-    var contact = {
-        firstName: "Atilla",
-        lastName: "Szikora",
-        city: "Miskolc",
-        address: "Katalin u. 7",
-        email: "szikora.atilla@gmail.com",
-        phone: "+36309728668",
-        sum: function () {
-            return "<div>" +
-                "<p>" + this.firstName + " " + this.lastName + "</p>" +
-                "<p>" + this.city + ", " + this.address + "</p>" +
-                "<p>" + this.email + "</p>" +
-                "<p>" + this.phone + "</p>" +
-                "</div>" } };
-
-    $.when(stateChange()).then(console.log("Masodik"));
+    xmlHttp.addEventListener("readystatechange", httpStateChange);
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
     createCards();
 });
