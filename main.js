aca71f582ef575fff16f3657135e6ae5890a8e6e
AtillaSzikora <szikora.atilla@gmail.com>
2016-06-02 09:00:22 +0200

    JSON handling fixed

--- main.js
+++ main.js
@@ -1,43 +1,34 @@
 $(document).ready(function() {
-    var json;
-    var response;
     var xmlHttp = new XMLHttpRequest();
     var url = "http://www.filltext.com/?rows=1&" +
         "firstName={firstName}&lastName={lastName}&" +
         "city={city}&address={streetAddress}&" +
         "email={email}&phone={phone|format}";
 
-    function stateChange() {
-        console.log("Elso");
+    function httpStateChange() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            response = xmlHttp.responseText;
-            json = JSON.parse(response);
-            console.log(json);
-             } }
+            var response = xmlHttp.responseText;
+            var jsonObj = JSON.parse(response);
+                createContact(jsonObj); } }
 
-    xmlHttp.addEventListener("readystatechange", stateChange);
+    function createContact(jsonObj) {
+        var contact = {
+            firstName: jsonObj[0].firstName,
+            lastName: jsonObj[0].lastName,
+            city: "Miskolc",
+            address: "Katalin u. 7",
+            email: "szikora.atilla@gmail.com",
+            phone: "+36309728668",
+            sum: function () {
+                return "<div>" +
+                    "<p>" + this.firstName + " " + this.lastName + "</p>" +
+                    "<p>" + this.city + ", " + this.address + "</p>" +
+                    "<p>" + this.email + "</p>" +
+                    "<p>" + this.phone + "</p>" +
+                    "</div>" } };
+        $('body').append(contact.sum()); }
+
+    xmlHttp.addEventListener("readystatechange", httpStateChange);
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
-
-    function createCards() {
-        for (var i = 0; i < 5; i++) {
-            $('body').append(contact.sum()); } }
-
-    var contact = {
-        firstName: "Atilla",
-        lastName: "Szikora",
-        city: "Miskolc",
-        address: "Katalin u. 7",
-        email: "szikora.atilla@gmail.com",
-        phone: "+36309728668",
-        sum: function () {
-            return "<div>" +
-                "<p>" + this.firstName + " " + this.lastName + "</p>" +
-                "<p>" + this.city + ", " + this.address + "</p>" +
-                "<p>" + this.email + "</p>" +
-                "<p>" + this.phone + "</p>" +
-                "</div>" } };
-
-    $.when(stateChange()).then(console.log("Masodik"));
-    createCards();
 });
